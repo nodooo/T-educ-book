ea9d61da1e7b79458cc7a52a07cf5b8464da8e8f
nodooo <48154459+nodooo@users.noreply.github.com>
2022-02-09 15:24:46 +0400

    Add files via upload

---
+++ tablet-anim-desctop1920-1/index.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'tablet-anim-desctop1920-1/data.json'
+})
